2e10f5620a8e71d56cbc49cae509c5f2a9c8771e
Pierre Plourde <pierre@spartancsharp.net>
2024-02-28 11:23:33 -0500

    Create Code
    Add students to a class roster List until there are no more students. Then print out the count of the students to the Console.

--- ListsConsoleApp/Program.cs
+++ ListsConsoleApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> classRoster = new List<string>();
+bool done = false;
+
+do
+{
+	Console.Write("Enter student name: ");
+	string? studentName = Console.ReadLine();
+
+	if ( !string.IsNullOrEmpty(studentName) )
+	{
+		classRoster.Add(studentName);
+
+	}
+
+	Console.Write("Any more students (yes/no): ");
+	string? moreStudents = Console.ReadLine();
+
+	if ( moreStudents?.ToLower() == "no" )
+	{
+		done = true;
+	}
+} while ( !done );
+
+Console.WriteLine($"There are {classRoster.Count} students in the class.");
